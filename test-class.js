eda43120eb7b1f4dde629581916b1be01b69e58e
Ben Madley <ben.madley@madetech.com>
2025-01-28 11:58:29 +0000

    Checkpoint.

--- test-class.js
+++ test-class.js
@@ -1,5 +1,25 @@
-export class TestClass{
-    aMethod(){
-        return true;
+export class Teaspoon {
+    asTeaspoons() {
+        return 1
+    }
+}
+
+export class Tablespoon {
+    asTeaspoons() {
+        return 3
+    }
+}
+
+export class Volume {
+    constructor(number, unit) {
+        this.number = number
+        this.unit = unit
+    }
+
+    equals(other) {
+        const thisTeaspoons = this.unit.asTeaspoons() * this.number
+        const otherTeaspoons = other.unit.asTeaspoons() * other.number
+
+        return thisTeaspoons === otherTeaspoons
     }
 }
--- test-class.test.js
+++ test-class.test.js
@@ -1,7 +1,34 @@
-import {TestClass} from "./test-class";
+import { Volume, Teaspoon, Tablespoon } from "./test-class";
 
-describe('TestClass...', () => {
-    test('', () => {
-        expect(new TestClass().aMethod()).toBe(true);
+const TEASPOON = new Teaspoon();
+const TABLESPOON = new Tablespoon();
+
+describe('Volume', () => {
+    test('one teaspoon equals one teaspoon', () => {
+        expect(
+            new Volume(1, TEASPOON)
+                .equals(new Volume(1, TEASPOON))
+        )
+            .toBe(true)
+    })
+
+    test("1 teaspoon doesn't equal 2 teaspoons", () => {
+        expect(new Volume(1, TEASPOON).equals(new Volume(2, TEASPOON))).toBe(false)
+    })
+
+    test("1 teaspoon doesn't equal 1 tablespoon", () => {
+        expect(new Volume(1, TEASPOON).equals(new Volume(1, TABLESPOON))).toBe(false)
+    })
+
+    test("3 teaspoon equals 1 tablespoon", () => {
+        expect(new Volume(3, TEASPOON).equals(new Volume(1, TABLESPOON))).toBe(true)
     })
 })
+
+// 7 classes unit e.g. tablespoons  
+// volume (1 class)  
+// 1 teaspoon equals 1 teaspoon  
+// equals  
+// return bool  
+// adds  
+// returns a measurement  
